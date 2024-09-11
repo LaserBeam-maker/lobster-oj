d8ee2e980cea1e926e9245a1bda3c9f730cb282e
laserbeam <yzm113042@gmail.com>
2024-09-12 06:13:59 +0800

    init

---
+++ lobsteroj-code-sandbox/src/main/java/com/yao/lobsterojcodesandbox/JavaNativeCodeSandbox.java
@@ -0,0 +1,17 @@
+package com.yao.lobsterojcodesandbox;
+
+import com.yao.lobsterojcodesandbox.model.ExecuteCodeRequest;
+import com.yao.lobsterojcodesandbox.model.ExecuteCodeResponse;
+import org.springframework.stereotype.Component;
+
+/**
+ * Java 原生代码沙箱实现（直接复用模板方法）
+ */
+@Component
+public class JavaNativeCodeSandbox extends JavaCodeSandboxTemplate {
+
+    @Override
+    public ExecuteCodeResponse executeCode(ExecuteCodeRequest executeCodeRequest) {
+        return super.executeCode(executeCodeRequest);
+    }
+}
---
+++ lobsteroj-code-sandbox/src/test/java/com/yao/lobsterojcodesandbox/YuojCodeSandboxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yao.lobsterojcodesandbox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YuojCodeSandboxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
